1220800bdefebad1c344f1fc82a9ef173edb8432
donghyun <gimdh1218@naver.com>
2020-09-10 20:12:01 +0900

    Notice model 생성, community page 수정

--- page/views.py
+++ page/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Notice
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def community(request):
+    notices = Notice.objects.all()
     return render(request, 'community.html')
