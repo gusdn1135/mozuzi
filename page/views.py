cbb5464f23b8ea6bbcfda77f2037ce1966591cb3
donghyun <gimdh1218@naver.com>
2020-09-17 20:56:02 +0900

    community 페이지 삭제

--- page/views.py
+++ page/views.py
@@ -5,10 +5,6 @@ from .models import Notice
 def home(request):
     return render(request, 'home.html')
 
-def community(request):
-    notices = Notice.objects.all()
-    return render(request, 'community.html')
-
 def story(request):
     return render(request, 'story.html')
 
